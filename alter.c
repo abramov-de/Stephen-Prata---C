a0bec5dccc2bccc9e6ef552bf4733cf5010ca6a0
abramov-d <abramov.de@phystech.edu>
2022-08-28 16:16:18 +0300

    Задачи из учебника.

---
+++ alter.c
@@ -0,0 +1,24 @@
+```c
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int alter(int * x, int * y);
+
+int main(void) {
+	int x, y;
+	printf("Vvedite dva chisla:\n");
+	scanf("%d %d", &x, &y);
+	printf("Pervonachalno: x = %d, y = %d.\n", x, y);
+	alter(&x, &y);
+	printf("Teper: x = %d, y = %d.\n", x, y);
+}
+
+int alter(int * x, int * y) {
+	int sum;
+	int dif;
+	sum = *x + *y;
+	dif = *x - *y;
+	*x = sum;
+	*y = dif;
+}
+```
