7b54d0414bc29f14b654352fd4d88e2b636f70bd
abramov-de <abramov.de@phystech.edu>
2022-08-30 11:27:06 +0300

    Функции и указатели

---
+++ swap3.c
@@ -0,0 +1,21 @@
+```c
+/* swap3.c -- использование указателей для обмена значениями переменных */
+#include<stdio.h>
+
+void interchange(int * u, int * v);
+
+int main(void) {
+	int x = 5, y = 10;
+	printf("Первоначально x = %d и y = %d.\n", x, y);
+	interchange(&x, &y);			//передача адресов в функцию
+	printf("Теперь x = %d и y = %d.\n", x, y);
+
+	return 0;
+}
+void intercange(int * u, int * v) {
+	int temp;
+	temp = *u;
+	*u = *v;
+	*v = temp;
+}
+```
