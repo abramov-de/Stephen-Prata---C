4dc7819324d9bd2617e33a238ff5e180e8ceb6d8
abramov-de <abramov.de@phystech.edu>
2022-08-30 11:23:29 +0300

    Функции и указатели

---
+++ harmonic_mean.c
@@ -0,0 +1,23 @@
+```c
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+double harmonic_mean(double a, double b);
+
+int main(void) {
+	double x, y;
+	printf("Vvedite dva tselih chisla:\n");
+	scanf("%f %f", &x, &y);
+	printf("Srednim garmonicheskim dvuh chisel %f i %f yavlyaetsya %f", x, y, harmonic_mean(x, y));
+
+	return 0;
+}
+
+double harmonic_mean(double a, double b) {
+	double arithmetic_mean;
+	double ans;
+	arithmetic_mean = ((1 / a + 1 / b) / 2);
+	ans = 1 / arithmetic_mean;
+	return ans;
+}
+```
